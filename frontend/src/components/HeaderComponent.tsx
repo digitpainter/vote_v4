a0a313d027283431e4777cdc2f2b095af328f222
kjuanchen <kjuanchen@tencent.com>
2025-03-24 20:58:05 +0800

    优化代码

--- frontend/src/components/HeaderComponent.tsx
+++ frontend/src/components/HeaderComponent.tsx
@@ -12,7 +12,7 @@ export function HeaderComponent() {
        bg-no-repeat md:bg-[url('/image/mobile_banner.svg')] bg-[url('/image/Banner.svg')] z-10"
     >
       <div className="flex flex-col items-center mt-[80px]">
-        <img src="/image/logo.svg" className="w-[256px] h-[68px] mb-6"/>
+        <img src="/image/logo.svg" className="w-[256px] h-[68px] mb-6" alt={"NUAA_LOGO"}/>
         <div className="text-white drop-shadow-md max-md:text-[30px] md:text-[48px]
          text-center w-full tracking-wider font-serif font-bold">
           {activeActivities?.length > 0 ? activeActivities[0].title : '活动标题'}
